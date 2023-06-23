7490e66987490817434b3a83798f0dd4e53b0ca7
Geoffrey Jensen <geoffreyjensen6@gmail.com>
2023-06-22 22:07:00 -0700

    Sprint 3 Complete

--- base_external/package/fsr/fsr.mk
+++ base_external/package/fsr/fsr.mk
@@ -5,24 +5,18 @@
 #
 ##############################################################
 
-FSR_VERSION = 'e2feb7549ed8e41aa48a73f06894e3abb9e01213'
+FSR_VERSION = '74a8658abb816f35c765cd38ffa4f61ec8254b67'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 FSR_SITE = 'git@github.com:geoffreyjensen6/final-project-fsr-driver.git'
 FSR_SITE_METHOD = git
 FSR_GIT_SUBMODULES = YES
-#FSR_MODULE_SUBDIRS = fsr_driver
 FSR_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
 
-#define FSR_BUILD_CMDS
-#        $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/test_script
-#endef
-
-#define FSR_INSTALL_TARGET_CMDS
-#        $(INSTALL) -m 0755 $(@D)/test_script/test_file $(TARGET_DIR)/etc/
-#endef
-
+define FSR_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/fsr-module-start-stop $(TARGET_DIR)/etc/init.d/S982fsrmodule
+endef
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
--- base_external/package/spice-rack-app/spice-rack-app.mk
+++ base_external/package/spice-rack-app/spice-rack-app.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-SPICE_RACK_APP_VERSION = 'd2b0bcf00695caf11b68cfe43369275b0561548d'
+SPICE_RACK_APP_VERSION = '03d44419e23b248fcde1824cca7fb3c029607b0c'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -15,10 +15,16 @@ SPICE_RACK_APP_GIT_SUBMODULES = YES
 
 define SPICE_RACK_APP_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) 
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server
 endef
 
 define SPICE_RACK_APP_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/spice_rack_app $(TARGET_DIR)/etc/spice_rack_app
+	$(INSTALL) -d 0755 $(@D)/ $(TARGET_DIR)/usr/bin/spice_rack
+	$(INSTALL) -m 0755 $(@D)/spice_conversions.csv $(TARGET_DIR)/usr/bin/spice_rack/spice_conversions.csv
+	$(INSTALL) -m 0755 $(@D)/spice_rack_app $(TARGET_DIR)/usr/bin/spice_rack/spice_rack_app
+	$(INSTALL) -m 0755 $(@D)/spice-rack-start-stop $(TARGET_DIR)/etc/init.d/S983SpiceRackApp
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket_server $(TARGET_DIR)/usr/bin/spice_rack/aesdsocket_server
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S984aesdsocket_server
 endef
 
 $(eval $(generic-package))
